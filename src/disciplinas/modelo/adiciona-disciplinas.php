653229c5c87ecac91eb460051a379acdce80cdef
giovanamds <giovana.silva247@etec.sp.gov.br>
2020-07-23 13:49:06 -0300

    commit projeto - 23.07.2020

--- src/disciplinas/modelo/adiciona-disciplinas.php
+++ src/disciplinas/modelo/adiciona-disciplinas.php
@@ -6,12 +6,20 @@
     $disciplina = $_REQUEST['disciplina'];
     $professor = $_REQUEST['professor'];
 
-    if($desciplina = ""){
+    if($disciplina = ""){
         echo "O campo com o nome da disciplina não pode estar vazio, tente novamente";
     }
     else{
         // Gerar um script SQL para cadastro das informações no banco de dados
-        $sql = "INSERT INTO disciplinas (disciplina, professor) VALUES ()";
+        $sql = "INSERT INTO disciplinas (disciplina, professor) VALUES ('".$disciplina."', '".$professor."')";
+
+        // Testar o comando SQL no banco de dados
+        if(mysqli_query($conecta, $sql)){
+            echo "Dados cadastrados com sucesso!";
+        }
+        else{
+            echo "Cadastro não efetuado";
+        }
     }
 
     
