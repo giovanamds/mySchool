922da607db1cb7b2b37e420fea12dcaf8a87cae7
giovanamds <giovana.silva247@etec.sp.gov.br>
2020-08-10 16:55:32 -0300

    commit projeto parte 4 - 10.08.2020

--- src/disciplinas/controle/list-disciplinas.js
+++ src/disciplinas/controle/list-disciplinas.js
@@ -11,19 +11,20 @@ $(document).ready(function() {
 
                 let resultado = `
                     <tr>
-                        <td>${dados[i].id}</td>
+                        <td class="text-center">${dados[i].id}</td>
                         <td>${dados[i].disciplina}</td>
                         <td>${dados[i].professor}</td>
-                        <td>
-                            <button class="btn btn-info btn-view btn-sm"><i class="mdi mdi-eye"></i></button>
-                            <button class="btn btn-primary btn-edit btn-sm"><i class="mdi mdi-pencil"></i></button>
-                            <button class="btn btn-danger btn-delete btn-sm"><i class="mdi mdi-trash-can"></i></button>
+                        <td class="text-center">
+                            <button id="${dados[i].id}" class="btn btn-info btn-view btn-sm"><i class="mdi mdi-eye"></i></button>
+                            <button id="${dados[i].id}" class="btn btn-primary btn-edit btn-sm"><i class="mdi mdi-pencil"></i></button>
+                            <button id="${dados[i].id}" class="btn btn-danger btn-delete btn-sm"><i class="mdi mdi-trash-can"></i></button>
                         </td>
                     </tr>
                     `
 
                 $('tbody').append(resultado)
             }
+            $('body').append('<script src="src/disciplinas/controle/view-disciplinas.js"></script>')
         }
     })
 
---
+++ src/disciplinas/controle/view-disciplinas.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+    // Monitorar o clique em cima dos botões com a classe btn-view
+    $('.btn-view').on('click', function(e) {
+
+        // Criando uma variável para coletar o ID do botão clicado
+        //Desenvolvo uma hash e juntamente com ela coleto o id do botão com a função this
+        var dados = `id = ${$(this).attr('id')}`
+
+        $.ajax({
+            type: 'POST',
+            dataType: 'JSON',
+            assync: true,
+            url: 'src/disciplinas/modelo/view-disciplinas.php',
+            success: function(dados) {
+
+                // Carregando meu formulário dentro da DIV que deixamos em branco para mostrar os dados
+                $('#form').load('src/disciplinas/visao/adiciona-disciplinas.html', function() {
+                    $('.btn-save').after('<button class="btn btn-secondary btn-block btn-close"> <i class="mdi mdi-close"></i> Fechar </button>')
+                    $('.btn-save').hide()
+                    $('h2').empty()
+                    $('h2').append('Visualização de cadastro')
+                    $('#disciplina').val(dados[0].disciplina)
+                    $('#disciplina').attr('disabled', true)
+                    $('#professor').val(dados[0].professor)
+                    $('#professor').attr('disabled', true)
+                })
+            }
+        })
+    })
+})
